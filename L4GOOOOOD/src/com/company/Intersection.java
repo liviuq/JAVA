97fd1aa791485828492f8ff9079cf88e7abebd77
Petrache Liviu Andrei <petrache.andrei1@gmail.com>
2022-03-18 11:12:19 +0200

    Added the compulsory part

---
+++ L4GOOOOOD/src/com/company/Intersection.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Intersection {
+
+    private String name;
+
+    public Intersection(String name)
+    {
+        this.name = name;
+    }
+}
---
+++ L4GOOOOOD/src/com/company/Street.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Street {
+
+    private String name;
+    private float length;
+    private Intersection intersection1;
+    private Intersection intersection2;
+
+
+}
