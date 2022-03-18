4434641184a1a80ec4ba5a95631cc47b90325c0b
Petrache Liviu Andrei <petrache.andrei1@gmail.com>
2022-03-18 11:35:44 +0200

    Added streets

--- L4GOOOOOD/src/com/company/Main.java
+++ L4GOOOOOD/src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
         //creating the intersections
-        var intersections = IntStream.rangeClosed(0,5)
+        var intersections = IntStream.rangeClosed(0,8)
                 .mapToObj(i -> new Intersection("i" + i))
                 .toList();
 
@@ -14,5 +16,11 @@ public class Main {
         {
             System.out.printf("%s\n", intersection);
         }
+
+        //creating the streets
+        List<Street> streets = new ArrayList<>();
+        streets.add(new Street("s1", 2, intersections.get(0), intersections.get(1)));
+
+        System.out.printf("%s\n", streets.get(0));
     }
 }
