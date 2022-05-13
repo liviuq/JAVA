66894530e541705b0a86b855260192114df209d2
Andrei Petrache <petrache.andrei1@gmail.com>
2022-05-13 11:01:20 +0300

    Completed compulsory for L10

---
+++ L10/src/main/java/app/Server.java
@@ -0,0 +1,30 @@
+package app;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server
+{
+    // Define the port on which the server is listening
+    public static final int PORT = 8100;
+    public Server() throws IOException {
+        ServerSocket serverSocket = null ;
+        try {
+            serverSocket = new ServerSocket(PORT);
+            while (true) {
+                System.out.println ("Waiting for a client ...");
+                Socket socket = serverSocket.accept();
+                // Execute the client's request in a new thread
+                new ClientThread(socket).run();
+            }
+        } catch (IOException e) {
+            System.err. println ("Ooops... " + e);
+        } finally {
+            serverSocket.close();
+        }
+    }
+    public static void main ( String [] args ) throws IOException {
+        Server server = new Server();
+    }
+}
