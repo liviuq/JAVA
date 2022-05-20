a0c611ca9857d340ad16e175ff69606cfbadb94b
Andrei Petrache <petrache.andrei1@gmail.com>
2022-05-20 11:01:53 +0300

    Updated L11

--- L11/src/main/java/com/uaic/L11/L11Application.java
+++ L11/src/main/java/com/uaic/L11/L11Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class L11Application {
-
+public class L11Application
+{
 	public static void main(String[] args) {
 		SpringApplication.run(L11Application.class, args);
 	}
-
 }
---
+++ L11/src/main/java/com/uaic/L11/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.uaic.L11.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @RequestMapping("/hello")
+    public String sayHello()
+    {
+        return "Greetings from Spring Boot!";
+    }
+}
